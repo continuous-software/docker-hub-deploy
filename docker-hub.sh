2666913c40fcefd10f5c9839cf26d1be868affb1
Lorenzofox3 <laurent34azerty@gmail.com>
2015-12-10 16:03:32 +0700

    first commit

---
+++ docker-hub.sh
@@ -0,0 +1,21 @@
+#/bin/bash
+
+VERSION=$(node -e "console.log(require('./package.json').version)")
+MAJOR=$(node -e "console.log(require('./package.json').version.split('.').slice(0,2).join('.'))")
+NAME=$(node -e "console.log(require('./package.json').name)")
+REGISTRY=$(node -e "console.log(require('./package.json').docker.registry)")
+USER=$(node -e "console.log(require('./package.json').docker.username)")
+PASS=$(node -e "console.log(require('./package.json').docker.password)")
+EMAIL=$(node -e "console.log(require('./package.json').docker.email)")
+
+echo "$VERSION"
+echo "$MAJOR"
+echo "login"
+docker login -u="$USER" -p="$PASS" -e="$EMAIL"
+echo "build start"
+docker build -t="$REGISTRY/$NAME" .
+
+docker tag -f $REGISTRY/$NAME $REGISTRY/$NAME:$VERSION
+docker tag -f $REGISTRY/$NAME $REGISTRY/$NAME:$MAJOR
+docker tag -f $REGISTRY/$NAME $REGISTRY/$NAME:latest
+docker push $REGISTRY/$NAME
